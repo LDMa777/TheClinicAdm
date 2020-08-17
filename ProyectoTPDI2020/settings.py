e5bf35a8ab54231d1e005091e6104645edbf7218
Your Name <you@example.com>
2020-08-17 16:17:29 -0300

    se modifica settings

--- ProyectoTPDI2020/settings.py
+++ ProyectoTPDI2020/settings.py
@@ -122,4 +122,9 @@ STATICFILES_DIRS = [
     BASE_DIR + "/ProyectoTPDI2020/static/"
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
